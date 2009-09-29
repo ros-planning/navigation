a6b4a958cc84db537d12d5bbbaf298898c2d3738
eitan <eitan@eb33c2ac-9c88-4c90-87e0-44a10359b0c3>
2009-09-29 01:21:38 +0000

    Whoops... actually I didn't... just thought I did... I need to eat something

--- tutorials/odometry_publisher_tutorial/src/odometry_publisher.cpp
+++ tutorials/odometry_publisher_tutorial/src/odometry_publisher.cpp
@@ -92,6 +92,7 @@ int main(int argc, char** argv){
     nav_msgs::Odometry odom;
     odom.header.stamp = current_time;
     odom.header.frame_id = "odom";
+    odom.child_frame_id = "base_link";
 
     //set the position
     odom.pose.pose.position.x = x;
