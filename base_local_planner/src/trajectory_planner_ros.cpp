459c14c688a0192fe41589470a9cde1835449311
eitan <eitan@eb33c2ac-9c88-4c90-87e0-44a10359b0c3>
2009-10-12 18:17:48 +0000

    Publishing orientation information as well in the local plan

--- base_local_planner/src/trajectory_planner_ros.cpp
+++ base_local_planner/src/trajectory_planner_ros.cpp
@@ -568,9 +568,7 @@ namespace base_local_planner {
 
     // Extract the plan in world co-ordinates, we assume the path is all in the same frame
     for(unsigned int i=0; i < path.size(); i++){
-      gui_path.poses[i].pose.position.x = path[i].pose.position.x;
-      gui_path.poses[i].pose.position.y = path[i].pose.position.y;
-      gui_path.poses[i].pose.position.z = path[i].pose.position.z;
+      gui_path.poses[i] = path[i];
     }
 
     pub.publish(gui_path);
