36d5545e2871b66e5dba6d36a6875a1ebd06eb47
eitan <eitan@eb33c2ac-9c88-4c90-87e0-44a10359b0c3>
2010-01-27 21:06:23 +0000

    Adding a warning if the inflation_radius parameter is less than the radii of the robot.

--- costmap_2d/src/costmap_2d_ros.cpp
+++ costmap_2d/src/costmap_2d_ros.cpp
@@ -246,6 +246,10 @@ namespace costmap_2d {
     //load the robot footprint from the parameter server if its available in the global namespace
     footprint_spec_ = loadRobotFootprint(private_nh, inscribed_radius, circumscribed_radius);
 
+    if(inscribed_radius > inflation_radius || circumscribed_radius > inflation_radius){
+      ROS_WARN("You have set an inflation radius that is less than the inscribed and circumscribed radii of the robot. This is dangerous and could casue the robot to hit obstacles. Please change your inflation radius setting appropraitely.");
+    }
+
     if(footprint_spec_.size() > 2){
       //now we need to compute the inscribed/circumscribed radius of the robot from the footprint specification
       double min_dist = std::numeric_limits<double>::max();
