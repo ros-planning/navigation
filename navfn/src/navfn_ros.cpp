5e9018eaa7d2533705144bda71dd4ec9e536584f
eitan <eitan@eb33c2ac-9c88-4c90-87e0-44a10359b0c3>
2010-05-21 21:14:31 +0000

    Moving away from deprecated apis in navfn

--- navfn/src/navfn_ros.cpp
+++ navfn/src/navfn_ros.cpp
@@ -328,7 +328,7 @@ namespace navfn {
 
     //create a message for the plan 
     nav_msgs::Path gui_path;
-    gui_path.set_poses_size(path.size());
+    gui_path.poses.resize(path.size());
     gui_path.header.frame_id = path[0].header.frame_id;
     gui_path.header.stamp = path[0].header.stamp;
 
