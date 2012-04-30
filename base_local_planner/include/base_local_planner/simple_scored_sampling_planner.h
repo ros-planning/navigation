562bafe04e6f259d11a1b0ae6ad2926192a64d82
Thibault Kruse <kruset@in.tum.de>
2012-04-30 10:45:56 -0700

    code into cpp file

--- base_local_planner/include/base_local_planner/simple_scored_sampling_planner.h
+++ base_local_planner/include/base_local_planner/simple_scored_sampling_planner.h
@@ -71,37 +71,13 @@ public:
    * passing max_samples = -1 (default): Each Sampling planner will continue to call
    * generator until generator runs out of samples (or forever if that never happens)
    */
-  SimpleScoredSamplingPlanner(std::vector<TrajectorySampleGenerator*> gen_list, std::vector<TrajectoryCostFunction*>& critics, int max_samples = -1) {
-    max_samples_ = max_samples;
-    gen_list_ = gen_list;
-    critics_ = critics;
-  }
+  SimpleScoredSamplingPlanner(std::vector<TrajectorySampleGenerator*> gen_list, std::vector<TrajectoryCostFunction*>& critics, int max_samples = -1);
 
   /**
    * runs all scoring functions over the trajectory creating a weigthed sum
    * of positive costs, or returns the first negative costs encountered if any.
    */
-  double scoreTrajectory(Trajectory& traj) {
-    double traj_cost = 0;
-    for(std::vector<TrajectoryCostFunction*>::iterator score_function = critics_.begin(); score_function != critics_.end(); ++score_function) {
-      TrajectoryCostFunction* score_function_p = *score_function;
-      if (score_function_p->getScale() == 0) {
-        continue;
-      }
-      double cost = score_function_p->scoreTrajectory(traj);
-      if (cost < 0) {
-        traj_cost = cost;
-        break;
-      }
-      if (cost != 0) {
-        cost *= score_function_p->getScale();
-      }
-      traj_cost += cost;
-    }
-
-
-    return traj_cost;
-  }
+  double scoreTrajectory(Trajectory& traj);
 
   /**
    * Calls generator until generator has no more samples or max_samples is reached.
@@ -114,68 +90,7 @@ public:
    * @param traj The container to write the result to
    * @param all_explored pass NULL or a container to collect all trajectories for debugging (has a penalty)
    */
-  bool findBestTrajectory(Trajectory& traj, std::vector<Trajectory>* all_explored) {
-    Trajectory loop_traj;
-    Trajectory best_traj;
-    double loop_traj_cost, best_traj_cost = -1;
-    bool gen_success;
-    int count, count_valid;
-    for (std::vector<TrajectoryCostFunction*>::iterator loop_critic = critics_.begin(); loop_critic != critics_.end(); ++loop_critic) {
-      TrajectoryCostFunction* loop_critic_p = *loop_critic;
-      if (loop_critic_p->prepare() == false) {
-        ROS_WARN("A scoring function failed to prepare");
-        return false;
-      }
-    }
-
-    for (std::vector<TrajectorySampleGenerator*>::iterator loop_gen = gen_list_.begin(); loop_gen != gen_list_.end(); ++loop_gen) {
-      count = 0;
-      count_valid = 0;
-      TrajectorySampleGenerator* gen_ = *loop_gen;
-      while (gen_->hasMoreTrajectories()) {
-        gen_success = gen_->nextTrajectory(loop_traj);
-        if (gen_success == false) {
-          // TODO use this for debugging
-          continue;
-        }
-        loop_traj_cost = scoreTrajectory(loop_traj);
-        if (all_explored != NULL) {
-          loop_traj.cost_ = loop_traj_cost;
-          all_explored->push_back(loop_traj);
-        }
-
-        if (loop_traj_cost >= 0) {
-          count_valid++;
-          if (best_traj_cost < 0 || loop_traj_cost < best_traj_cost) {
-            best_traj_cost = loop_traj_cost;
-            best_traj = loop_traj;
-          }
-        }
-        count++;
-        if (max_samples_ > 0 && count >= max_samples_) {
-          break;
-        }        
-      }
-      if (best_traj_cost >= 0) {
-        traj.xv_ = best_traj.xv_;
-        traj.yv_ = best_traj.yv_;
-        traj.thetav_ = best_traj.thetav_;
-        traj.cost_ = best_traj_cost;
-        traj.resetPoints();
-        double px, py, pth;
-        for (unsigned int i = 0; i < best_traj.getPointsSize(); i++) {
-          best_traj.getPoint(i, px, py, pth);
-          traj.addPoint(px, py, pth);
-        }
-      }
-      //ROS_DEBUG("Evaluated %d trajectories, found %d valid", count, count_valid);
-      if (best_traj_cost >= 0) {
-        // do not try fallback generators
-        break;
-      }
-    }
-    return best_traj_cost >= 0;
-  }
+  bool findBestTrajectory(Trajectory& traj, std::vector<Trajectory>* all_explored = NULL);
 
 
 private:
