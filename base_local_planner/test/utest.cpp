52b9fe8e7f22ad056dda7215c55e58fca7f682e2
Thibault Kruse <kruset@in.tum.de>
2012-05-02 16:56:54 -0700

    moved footprint helper tests to own file

--- base_local_planner/test/utest.cpp
+++ base_local_planner/test/utest.cpp
@@ -35,7 +35,7 @@
 #include <iostream>
 #include <vector>
 #include <utility>
-#include <base_local_planner/footprint_helper.h>
+
 #include <base_local_planner/map_cell.h>
 #include <base_local_planner/map_grid.h>
 #include <base_local_planner/trajectory.h>
@@ -47,167 +47,11 @@
 #include <geometry_msgs/Point.h>
 #include <base_local_planner/Position2DInt.h>
 
+#include "wavefront_map_accessor.h"
 
 using namespace std;
 
 namespace base_local_planner {
-
-/**
- * Map_grids rely on costmaps to identify obstacles. We need a costmap that we can easily manipulate for unit tests.
- * This class has a grid map where we can set grid cell state, and a synchronize method to make the costmap match.
- */
-class WavefrontMapAccessor : public costmap_2d::Costmap2D {
-  public:
-    WavefrontMapAccessor(MapGrid* map, double outer_radius)
-      : costmap_2d::Costmap2D(map->size_x_, map->size_y_, 1, 0, 0, 5, 10, 15),
-        map_(map), outer_radius_(outer_radius) {
-      synchronize();
-    }
-
-    virtual ~WavefrontMapAccessor(){};
-
-    void synchronize(){
-      // Write Cost Data from the map
-      for(unsigned int x = 0; x < size_x_; x++){
-        for (unsigned int y = 0; y < size_y_; y++){
-          unsigned int ind = x + (y * size_x_);
-          if(map_->operator ()(x, y).target_dist == 1) {
-            costmap_[ind] = costmap_2d::LETHAL_OBSTACLE;
-          } else {
-            costmap_[ind] = 0;
-          }
-        }
-      }
-    }
-
-  private:
-    MapGrid* map_;
-    double outer_radius_;
-};
-
-class FootprintHelperTest : public testing::Test {
-public:
-  FootprintHelper fh;
-
-  FootprintHelperTest() {
-  }
-
-  virtual void TestBody(){}
-
-  void correctLineCells() {
-    vector<base_local_planner::Position2DInt> footprint;
-    fh.getLineCells(0, 10, 0, 10, footprint);
-    EXPECT_EQ(11, footprint.size());
-    EXPECT_EQ(footprint[0].x, 0);
-    EXPECT_EQ(footprint[0].y, 0);
-    EXPECT_EQ(footprint[5].x, 5);
-    EXPECT_EQ(footprint[5].y, 5);
-    EXPECT_EQ(footprint[10].x, 10);
-    EXPECT_EQ(footprint[10].y, 10);
-  }
-
-  void correctFootprint(){
-    MapGrid* mg = new MapGrid (10, 10);
-    WavefrontMapAccessor* wa = new WavefrontMapAccessor(mg, .25);
-    const costmap_2d::Costmap2D& map = *wa;
-
-    std::vector<geometry_msgs::Point> footprint_spec;
-    geometry_msgs::Point pt;
-    //create a square footprint
-    pt.x = 2;
-    pt.y = 2;
-    footprint_spec.push_back(pt);
-    pt.x = 2;
-    pt.y = -2;
-    footprint_spec.push_back(pt);
-    pt.x = -2;
-    pt.y = -2;
-    footprint_spec.push_back(pt);
-    pt.x = -2;
-    pt.y = 2;
-    footprint_spec.push_back(pt);
-
-    Eigen::Vector3f pos(4.5, 4.5, 0);
-    //just create a basic footprint
-    vector<base_local_planner::Position2DInt> footprint = fh.getFootprintCells(pos, footprint_spec, map, false);
-
-    EXPECT_EQ(20, footprint.size());
-    //we expect the front line to be first
-    EXPECT_EQ(footprint[0].x, 6); EXPECT_EQ(footprint[0].y, 6);
-    EXPECT_EQ(footprint[1].x, 6); EXPECT_EQ(footprint[1].y, 5);
-    EXPECT_EQ(footprint[2].x, 6); EXPECT_EQ(footprint[2].y, 4);
-    EXPECT_EQ(footprint[3].x, 6); EXPECT_EQ(footprint[3].y, 3);
-    EXPECT_EQ(footprint[4].x, 6); EXPECT_EQ(footprint[4].y, 2);
-
-    //next the right line
-    EXPECT_EQ(footprint[5].x, 6); EXPECT_EQ(footprint[5].y, 2);
-    EXPECT_EQ(footprint[6].x, 5); EXPECT_EQ(footprint[6].y, 2);
-    EXPECT_EQ(footprint[7].x, 4); EXPECT_EQ(footprint[7].y, 2);
-    EXPECT_EQ(footprint[8].x, 3); EXPECT_EQ(footprint[8].y, 2);
-    EXPECT_EQ(footprint[9].x, 2); EXPECT_EQ(footprint[9].y, 2);
-
-    //next the back line
-    EXPECT_EQ(footprint[10].x, 2); EXPECT_EQ(footprint[10].y, 2);
-    EXPECT_EQ(footprint[11].x, 2); EXPECT_EQ(footprint[11].y, 3);
-    EXPECT_EQ(footprint[12].x, 2); EXPECT_EQ(footprint[12].y, 4);
-    EXPECT_EQ(footprint[13].x, 2); EXPECT_EQ(footprint[13].y, 5);
-    EXPECT_EQ(footprint[14].x, 2); EXPECT_EQ(footprint[14].y, 6);
-
-    //finally the left line
-    EXPECT_EQ(footprint[15].x, 2); EXPECT_EQ(footprint[15].y, 6);
-    EXPECT_EQ(footprint[16].x, 3); EXPECT_EQ(footprint[16].y, 6);
-    EXPECT_EQ(footprint[17].x, 4); EXPECT_EQ(footprint[17].y, 6);
-    EXPECT_EQ(footprint[18].x, 5); EXPECT_EQ(footprint[18].y, 6);
-    EXPECT_EQ(footprint[19].x, 6); EXPECT_EQ(footprint[19].y, 6);
-
-
-    pos = Eigen::Vector3f(4.5, 4.5, M_PI_2);
-    //check that rotation of the footprint works
-    footprint = fh.getFootprintCells(pos, footprint_spec, map, false);
-
-    //first the left line
-    EXPECT_EQ(footprint[0].x, 2); EXPECT_EQ(footprint[0].y, 6);
-    EXPECT_EQ(footprint[1].x, 3); EXPECT_EQ(footprint[1].y, 6);
-    EXPECT_EQ(footprint[2].x, 4); EXPECT_EQ(footprint[2].y, 6);
-    EXPECT_EQ(footprint[3].x, 5); EXPECT_EQ(footprint[3].y, 6);
-    EXPECT_EQ(footprint[4].x, 6); EXPECT_EQ(footprint[4].y, 6);
-
-    //next the front line
-    EXPECT_EQ(footprint[5].x, 6); EXPECT_EQ(footprint[5].y, 6);
-    EXPECT_EQ(footprint[6].x, 6); EXPECT_EQ(footprint[6].y, 5);
-    EXPECT_EQ(footprint[7].x, 6); EXPECT_EQ(footprint[7].y, 4);
-    EXPECT_EQ(footprint[8].x, 6); EXPECT_EQ(footprint[8].y, 3);
-    EXPECT_EQ(footprint[9].x, 6); EXPECT_EQ(footprint[9].y, 2);
-
-    //next the right line
-    EXPECT_EQ(footprint[10].x, 6); EXPECT_EQ(footprint[10].y, 2);
-    EXPECT_EQ(footprint[11].x, 5); EXPECT_EQ(footprint[11].y, 2);
-    EXPECT_EQ(footprint[12].x, 4); EXPECT_EQ(footprint[12].y, 2);
-    EXPECT_EQ(footprint[13].x, 3); EXPECT_EQ(footprint[13].y, 2);
-    EXPECT_EQ(footprint[14].x, 2); EXPECT_EQ(footprint[14].y, 2);
-
-    //next the back line
-    EXPECT_EQ(footprint[15].x, 2); EXPECT_EQ(footprint[15].y, 2);
-    EXPECT_EQ(footprint[16].x, 2); EXPECT_EQ(footprint[16].y, 3);
-    EXPECT_EQ(footprint[17].x, 2); EXPECT_EQ(footprint[17].y, 4);
-    EXPECT_EQ(footprint[18].x, 2); EXPECT_EQ(footprint[18].y, 5);
-    EXPECT_EQ(footprint[19].x, 2); EXPECT_EQ(footprint[19].y, 6);
-  }
-
-};
-
-
-TEST(TrajectoryPlannerTest, correctFootprint){
-  FootprintHelperTest tct;
-  tct.correctFootprint();
-}
-
-TEST(TrajectoryPlannerTest, correctLineCells){
-  FootprintHelperTest tct;
-  tct.correctLineCells();
-}
-
-
 
 class TrajectoryPlannerTest : public testing::Test {
   public:
