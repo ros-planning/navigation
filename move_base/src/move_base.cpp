fe7980d4cec6da92263d1fa482bde1ecc2612b33
Dave Hershberger <hersh@willowgarage.com>
2013-02-01 16:48:49 -0800

    move_base: moved main() into separate cpp file.

--- move_base/src/move_base.cpp
+++ move_base/src/move_base.cpp
@@ -1144,16 +1144,3 @@ namespace move_base {
   }
 
 };
-
-int main(int argc, char** argv){
-  ros::init(argc, argv, "move_base_node");
-  tf::TransformListener tf(ros::Duration(10));
-
-  move_base::MoveBase move_base("move_base", tf);
-
-  //ros::MultiThreadedSpinner s;
-  ros::spin();
-
-  return(0);
-
-}
