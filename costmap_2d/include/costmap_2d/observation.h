5f107b51422a23cbab51356d6d924f0c32b027ea
David Lu!! <davidvlu@gmail.com>
2013-04-02 16:35:02 -0700

    Change in BSD License

--- costmap_2d/include/costmap_2d/observation.h
+++ costmap_2d/include/costmap_2d/observation.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, Willow Garage, Inc.
+ * Copyright (c) 2008, 2013, Willow Garage, Inc.
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
