1cccde32b5ef1b29722a7fa64600582aaf2951ef
David Lu!! <davidvlu@gmail.com>
2013-04-02 17:15:18 -0700

    Update license

--- global_planner/src/plan_node.cpp
+++ global_planner/src/plan_node.cpp
@@ -2,7 +2,7 @@
  *
  * Software License Agreement (BSD License)
  *
- *  Copyright (c) 2008, Willow Garage, Inc.
+ *  Copyright (c) 2008, 2013, Willow Garage, Inc.
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
@@ -15,7 +15,7 @@
  *     copyright notice, this list of conditions and the following
  *     disclaimer in the documentation and/or other materials provided
  *     with the distribution.
- *   * Neither the name of the Willow Garage nor the names of its
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
  *     contributors may be used to endorse or promote products derived
  *     from this software without specific prior written permission.
  *
@@ -33,6 +33,7 @@
  *  POSSIBILITY OF SUCH DAMAGE.
  *
  * Author: Bhaskara Marthi
+ *         David V. Lu!!
  *********************************************************************/
 #include <global_planner/planner_core.h>
 #include <navfn/MakeNavPlan.h>
