3858707560d5c4a2b262a797dfd3b1e8e7596ad7
Dave Hershberger <hersh@willowgarage.com>
2013-05-20 14:35:22 -0700

    costmap_2d: moved inline function body into .h file to fix linker error

--- costmap_2d/plugins/inflation_layer.cpp
+++ costmap_2d/plugins/inflation_layer.cpp
@@ -224,25 +224,4 @@ void InflationLayer::deleteKernels()
   }
 }
 
-/**
- * @brief  Given a distance... compute a cost
- * @param  distance The distance from an obstacle in cells
- * @return A cost value for the distance
- */
-inline unsigned char InflationLayer::computeCost(double distance) const
-{
-  unsigned char cost = 0;
-  if (distance == 0)
-    cost = LETHAL_OBSTACLE;
-  else if (distance * resolution_ <= inscribed_radius_)
-    cost = INSCRIBED_INFLATED_OBSTACLE;
-  else
-  {
-    //make sure cost falls off by Euclidean distance
-    double euclidean_distance = distance * resolution_;
-    double factor = exp(-1.0 * weight_ * (euclidean_distance - inscribed_radius_));
-    cost = (unsigned char)((INSCRIBED_INFLATED_OBSTACLE - 1) * factor);
-  }
-  return cost;
-}
-}
+} // end namespace costmap_2d
