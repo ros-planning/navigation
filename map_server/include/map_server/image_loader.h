ed2f1830b3c72b18bfee0213ea0cd8026d0cf02b
Mr-Yellow <mr-yellow@mr-yellow.com>
2016-08-17 09:04:03 +1000

    Add legacy signature with MapMode as bool

--- map_server/include/map_server/image_loader.h
+++ map_server/include/map_server/image_loader.h
@@ -1,10 +1,10 @@
 /*
  * Copyright (c) 2008, Willow Garage, Inc.
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  *     * Redistributions of source code must retain the above copyright
  *       notice, this list of conditions and the following disclaimer.
  *     * Redistributions in binary form must reproduce the above copyright
@@ -13,7 +13,7 @@
  *     * Neither the name of the Willow Garage, Inc. nor the names of its
  *       contributors may be used to endorse or promote products derived from
  *       this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -54,8 +54,8 @@ enum MapMode {TRINARY, SCALE, RAW};
 namespace map_server
 {
 
-/** Read the image from file and fill out the resp object, for later 
- * use when our services are requested. 
+/** Read the image from file and fill out the resp object, for later
+ * use when our services are requested.
  *
  * @param resp The map wil be written into here
  * @param fname The image file to read from
@@ -72,6 +72,24 @@ void loadMapFromFile(nav_msgs::GetMap::Response* resp,
                      const char* fname, double res, bool negate,
                      double occ_th, double free_th, double* origin,
                      MapMode mode=TRINARY);
+
+/** Legacy signature see issue #474
+ *
+* @param resp The map wil be written into here
+* @param fname The image file to read from
+* @param res The resolution of the map (gets stored in resp)
+* @param negate If true, then whiter pixels are occupied, and blacker
+*               pixels are free
+* @param occ_th Threshold above which pixels are occupied
+* @param free_th Threshold below which pixels are free
+* @param origin Triple specifying 2-D pose of lower-left corner of image
+* @param trinary If true, only outputs Occupied/Free/Unknown
+* @throws std::runtime_error If the image file can't be loaded
+* */
+void loadMapFromFile(nav_msgs::GetMap::Response* resp,
+                    const char* fname, double res, bool negate,
+                    double occ_th, double free_th, double* origin,
+                    bool trinary=true);
 }
 
 #endif
