7b744c64e0e0346a61d0b3eb5abbda1074b55053
Dan Winkler <dan@6river.com>
2017-11-08 15:38:54 -0500

    fix: Fixed map pointer.

--- amcl/src/amcl/sensors/amcl_laser.cpp
+++ amcl/src/amcl/sensors/amcl_laser.cpp
@@ -554,10 +554,10 @@ void AMCLLaser::reallocTempData(int new_max_samples, int new_max_obs){
 }
 
 bool isValidSample(pf_sample_t* sample){
-  int x = MAP_GXWX(self->map, sample->pose.v[0]);
-  int y = MAP_GYWY(self->map, sample->pose.v[1]);
+  int x = MAP_GXWX(map, sample->pose.v[0]);
+  int y = MAP_GYWY(map, sample->pose.v[1]);
 
-  if(!MAP_VALID(self->map, x, y) || self->map->cells[MAP_INDEX(self->map, x, y)].occ_state > -1)
+  if(!MAP_VALID(map, x, y) || map->cells[MAP_INDEX(map, x, y)].occ_state > -1)
   {
     sample->weight = 0;
 
