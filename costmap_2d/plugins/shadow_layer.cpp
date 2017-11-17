433d6258843aae1b6637f16b328d74d58b82ae19
grieneis <dgrieneisen@6river.com>
2017-11-17 09:59:16 -0500

    feat: Initial shadow layer cpp file

---
+++ costmap_2d/plugins/shadow_layer.cpp
@@ -0,0 +1,356 @@
+/*********************************************************************
+ *
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2008, 2013, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Author: Eitan Marder-Eppstein
+ *         David V. Lu!!
+ *********************************************************************/
+#include <costmap_2d/shadow_layer.h>
+#include <costmap_2d/costmap_math.h>
+#include <pluginlib/class_list_macros.h>
+
+PLUGINLIB_EXPORT_CLASS(costmap_2d::ShadowLayer, costmap_2d::Layer)
+
+using costmap_2d::LETHAL_OBSTACLE;
+
+using costmap_2d::ObservationBuffer;
+using costmap_2d::Observation;
+
+namespace costmap_2d
+{
+
+void ShadowLayer::onInitialize()
+{
+  ros::NodeHandle nh("~/" + name_), g_nh;
+  rolling_window_ = layered_costmap_->isRolling();
+
+  default_value_ = UNKNOWN;
+
+  ShadowLayer::matchSize();
+  current_ = true;
+
+  global_frame_ = layered_costmap_->getGlobalFrameID();
+
+  dsrv_ = NULL;
+  setupDynamicReconfigure(nh);
+}
+
+void ShadowLayer::setupDynamicReconfigure(ros::NodeHandle& nh)
+{
+  dsrv_ = new dynamic_reconfigure::Server<costmap_2d::ShadowPluginConfig>(nh);
+  dynamic_reconfigure::Server<costmap_2d::ShadowPluginConfig>::CallbackType cb = boost::bind(
+      &ShadowLayer::reconfigureCB, this, _1, _2);
+  dsrv_->setCallback(cb);
+}
+
+ShadowLayer::~ShadowLayer()
+{
+    if (dsrv_)
+        delete dsrv_;
+}
+void ShadowLayer::reconfigureCB(costmap_2d::ObstaclePluginConfig &config, uint32_t level)
+{
+  enabled_ = config.enabled;
+  max_raytrace_range_ = config.max_raytrace_range;
+  min_shadow_size_ = config.min_shadow_size_;
+}
+
+
+void ShadowLayer::updateBounds(double robot_x, double robot_y, double robot_yaw, double* min_x,
+                                          double* min_y, double* max_x, double* max_y)
+{
+  if (rolling_window_) {
+    updateOrigin(robot_x - getSizeInMetersX() / 2, robot_y - getSizeInMetersY() / 2);
+  }
+
+  if (!enabled_) {
+    return;
+  }
+
+  // Bounds do not need to be updated b/c we are not adding cost to the costmap. yet.
+  /// TODO: update bounds when this functionality is added.
+
+
+  // generate the observation to be used
+  generateMaxShadowObservation();
+}
+
+void ShadowLayer::generateMaxShadowObservation(double robot_x, double robot_y, double robot_yaw,
+                                          double* min_x, double* min_y, double* max_x, double* max_y)
+{
+  // Should this be based on the lidar scan?
+  // For now, just generate some points.
+  shadow_observation_ = std::make_shared<Observation>();
+  
+  pcl::PointCloud <pcl::PointXYZ> &observation_cloud = *(shadow_observation_->cloud_);
+
+  unsigned int num_points = shadow_scan_half_angle_ / shadow_scan_angular_resolution_ * 2 + 1;
+  observation_cloud.reserve(num_points);
+
+  double angle_start = -shadow_scan_half_angle_ + robot_yaw;
+  double angle_end = shadow_scan_half_angle_ + robot_yaw;
+  for (double angle = angle_start; 
+    angle <= angle_end; 
+    angle += shadow_scan_angular_resolution_)
+  {
+    observation_cloud.push_back(pcl::PointXYZ(
+      cos(angle) * shadow_scan_range_ + robot_x,
+      sin(angle) * shadow_scan_range_ + robot_y,
+      0.0)
+    );
+  }
+}
+
+
+void ShadowLayer::updateCosts(costmap_2d::Costmap2D& master_grid, int min_i, int min_j, int max_i, int max_j)
+{
+  if (!enabled_) {
+    return;
+  }
+
+  calculateShadows(master_grid);
+}
+
+void ShadowLayer::calculateShadows(costmap_2d::Costmap2D& master_grid)
+{
+  // First, reset the costmap.
+  resetMaps();
+
+  // Raytrace the observations into the map
+  raytraceShadowObservation(costmap_2d::Costmap2D& master_grid);
+
+  // Calculate all of the spots where there could be a shadow.
+  calculateShadowSpots();
+}
+
+
+void ShadowLayer::raytraceShadowObservation(costmap_2d::Costmap2D& master_grid)
+{
+  // Clear out the old shadow points
+  shadowed_points_->clear();
+
+  // Check if the observation is valid.
+  if (!shadow_observation_)
+  {
+    ROS_WARN_THROTTLE(1.0, "Cannot raytrace in shadow layer because there is no shadow observation.");
+    return;
+  }
+
+  // 
+  double ox = observation.origin_.x;
+  double oy = observation.origin_.y;
+  pcl::PointCloud < pcl::PointXYZ > cloud = *(observation.cloud_);
+
+  // get the map coordinates of the origin of the sensor
+  unsigned int x0, y0;
+  if (!worldToMap(ox, oy, x0, y0))
+  {
+    ROS_WARN_THROTTLE(
+        1.0, "The origin for the sensor at (%.2f, %.2f) is out of map bounds. So, the costmap cannot raytrace for it.",
+        ox, oy);
+    return;
+  }
+
+  // we can pre-compute the enpoints of the map outside of the inner loop... we'll need these later
+  double origin_x = origin_x_, origin_y = origin_y_;
+  double map_end_x = origin_x + size_x_ * resolution_;
+  double map_end_y = origin_y + size_y_ * resolution_;
+
+  // get min_raytrace_range if a value is specified
+  double min_raytrace_dist = observation.min_raytrace_range_;
+
+  touch(ox, oy, min_x, min_y, max_x, max_y);
+
+  // for each point in the cloud, we want to trace a line from the origin and clear obstacles along it
+  for (unsigned int i = 0; i < cloud.points.size(); ++i)
+  {
+    double wx = cloud.points[i].x;
+    double wy = cloud.points[i].y;
+
+    double a = wx - ox;
+    double b = wy - oy;
+    double ray_length = sqrt(a * a + b * b);
+
+
+    // calculate raytrace starting point
+    // the raytrace range should be (rx, ry) -> (wx, wy)
+    double rx = ox;
+    double ry = oy;
+
+    // now we need to make sure that the point we're raytracing
+    // to isn't off the costmap and scale if necessary
+    checkRaytracePoint(origin_x, origin_y, map_end_x, map_end_y, ox, oy, wx, wy);
+    // now that the vector is scaled correctly... we'll get the map coordinates of its endpoint
+    unsigned int x1, y1;
+
+    // check for legality just in case
+    if (!worldToMap(wx, wy, x1, y1))
+      continue;
+
+    // same process for rx and ry
+    checkRaytracePoint(origin_x, origin_y, map_end_x, map_end_y, ox, oy, rx, ry);
+
+    unsigned int rx_map, ry_map;
+
+    // check for legality of the raytrace starting point
+    if (!worldToMap(rx, ry, rx_map, ry_map)){
+      continue;
+    }
+
+    unsigned int cell_raytrace_range = cellDistance(observation.raytrace_range_);
+
+    ShadowMarker(costmap_, master_grid, shadowed_points_,
+      VISIBLE, SHADOW, OBSTACLE);
+
+
+    // and finally... we can execute our trace to clear obstacles along that line
+    raytraceLine(marker, rx_map, ry_map, x1, y1, cell_raytrace_range);
+  }
+}
+
+
+  /// TODO - move to header.
+  class ShadowMarker
+  {
+  public:
+    ShadowMarker(unsigned char* costmap, unsigned char* master_grid,
+      std::shared_ptr<std::vector> shadow_points,
+      unsigned char visible_value_, unsigned char shadow_value_,
+      unsigned char obstacle_value_) :
+        costmap_(costmap), master_grid_(master_grid),
+        shadow_points_(shadow_points),
+        visible_value_(visible_value), shadow_value_(shadow_value),
+        obstacle_value_(obstacle_value),
+        passed_obstacle_(false)
+    {
+    }
+    inline void operator()(unsigned int offset)
+    {
+      if (master_grid_[offset] == costmap_2d::LETHAL_OBSTACLE)
+      {
+        costmap_[offset] = obstacle_value_;
+        passed_obstacle_ = true;
+      }
+      else if (passed_obstacle_)
+      {
+        costmap_[offset] = shadow_value_;
+        // Gather all the shadow points
+        shadow_points_->append(offset);
+      }
+      else
+      {
+        costmap_[offset] = visible_value_;
+      }
+    }
+  private:
+    unsigned char* costmap_;
+    unsigned char* master_grid_;
+    std::shared_ptr<std::vector<unsigned int>> shadow_points_;
+    unsigned char visible_value_;
+    unsigned char shadow_value_;
+    unsigned char obstacle_value_;
+    bool passed_obstacle_;
+
+  };
+
+void ShadowLayer::calculateShadowedObjetcs()
+{
+  shadowed_objects->clear();
+
+  // Iterate over all of the shadowed_points_
+  for (auto it : (*shadowed_points_))
+  {
+    if (checkForShadowedObjectAtIndex(*it))
+    {
+      shadowed_objects_->push_back(createPointFromIndex(*it));
+    }
+  }
+}
+
+bool ShadowLayer::checkForShadowedObjectAtIndex(unsigned int idx)
+{
+  unsigned int half_width = resolution_ * min_shadow_size_;
+  unsigned int idx_x, idx_y;
+  indexToCells(idx, idx_x, idx_y);
+
+  // calculate the square to check
+  unsigned int start_x = idx_x > half_width ? idx_x - half_width : 0;
+  unsigned int start_y = idx_y > half_width ? idx_y - half_width : 0;
+  unsigned int end_x = idx < getSizeInCellsX() - 1 - half_width ? idx_x + half_width : getSizeInCellsX() - 1;
+  unsigned int end_x = idx < getSizeInCellsY() - 1 - half_width ? idx_y + half_width : getSizeInCellsY() - 1;
+
+  // Iterate over the square and see if there are any CLEAR or OBSTACLE values
+  for (unsigned int y_val = start_y; y_val <= end_y; ++y_val)
+  {
+    for (unsigned int x_val = start_x; x_val <= end_x; ++x_val)
+    {
+      switch (costmap_[getIndex(x_val, y_val)])
+      {
+        case VISIBLE:
+        case OBSTACLE:
+          return false;
+        default:
+          continue;
+      }
+    }
+  }
+}
+
+geometry_msgs::Point ShadowLayer::createPointFromIndex(unsigned int idx)
+{
+  geometry_msgs::Point pt;
+  unsigned int mx, my;
+  indexToCells(idx, mx, my);
+  mapToWorld(mx, my, pt.x, pt.y);
+  return pt;
+}
+
+
+
+void ShadowLayer::updateRaytraceBounds(double ox, double oy, double wx, double wy, double range,
+                                         double* min_x, double* min_y, double* max_x, double* max_y)
+{
+  double dx = wx-ox, dy = wy-oy;
+  double full_distance = hypot(dx, dy);
+  double scale = std::min(1.0, range / full_distance);
+  double ex = ox + dx * scale, ey = oy + dy * scale;
+  touch(ex, ey, min_x, min_y, max_x, max_y);
+}
+
+void ShadowLayer::reset()
+{
+    resetMaps();
+    current_ = true;
+}
+
+}  // namespace costmap_2d
