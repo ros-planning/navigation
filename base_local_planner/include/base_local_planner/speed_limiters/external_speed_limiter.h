4762047c3256555bb8d3630dd21fdafd57952b44
grieneis <dgrieneisen@6river.com>
2017-12-05 19:10:30 -0500

    feat: Added external speed limiter as well (for zones).

---
+++ base_local_planner/include/base_local_planner/speed_limiters/external_speed_limiter.h
@@ -0,0 +1,90 @@
+/*********************************************************************
+ *
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2016, 6 River Systems
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of 6 River Systems. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Author: Daniel Grieneisen
+ *********************************************************************/
+
+#ifndef EXTERNAL_SPEED_LIMITER_H_
+#define EXTERNAL_SPEED_LIMITER_H_
+
+#include <base_local_planner/speed_limiters/speed_limiter.h>
+#include <base_local_planner/ExternalSpeedLimiterConfig.h>
+#include <base_local_planner/SpeedLimitRequest.h>
+
+namespace base_local_planner {
+
+/**
+ * This class slows down the robot from some external limit
+ *
+ */
+class ExternalSpeedLimiter : public SpeedLimiter {
+public:
+  /**
+   * Constructor
+   */
+  ExternalSpeedLimiter(costmap_2d::Costmap2DROS* costmap) : SpeedLimiter(costmap) {};
+
+  /**
+   * Destructor
+   */
+  virtual ~ExternalSpeedLimiter() {}
+
+  virtual void initialize(std::string name);
+
+  /**
+   * Prepare for operation.
+   * @return true if preparations were successful
+   */
+  virtual bool calculateLimits(double& max_allowed_linear_vel, double& max_allowed_angular_vel);
+
+private:
+  void reconfigure(ExternalSpeedLimiterConfig &cfg, uint32_t level) {
+    params_ = cfg;
+  }
+
+  void msgCallback(base_local_planner::SpeedLimitRequest msg) {
+    last_msg_ = msg;
+    last_msg_time_ = ros::Time::now();
+  }
+
+  std::shared_ptr<dynamic_reconfigure::Server<ExternalSpeedLimiterConfig>> configServer_;
+  ExternalSpeedLimiterConfig params_;
+
+  ros::Subscriber subscriber_;
+  base_local_planner::SpeedLimitRequest last_msg_;
+  ros::Time last_msg_time_ = ros::Time(0);
+};
+
+} /* namespace base_local_planner */
+#endif /* PATH_SPEED_LIMITER_H_ */
---
+++ base_local_planner/src/speed_limiters/external_speed_limiter.cpp
@@ -0,0 +1,76 @@
+/*********************************************************************
+ *
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2016, 6 River Systems
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Author: Daniel Grieneisen
+ *********************************************************************/
+
+#include <base_local_planner/speed_limiters/external_speed_limiter.h>
+#include <base_local_planner/geometry_math_helpers.h>
+#include <tf/transform_datatypes.h>
+
+namespace base_local_planner {
+
+void ExternalSpeedLimiter::initialize(std::string name) {
+  ros::NodeHandle private_nh(name + "/external");
+  configServer_ = std::make_shared<dynamic_reconfigure::Server<ExternalSpeedLimiterConfig>>(private_nh);
+  configServer_->setCallback(boost::bind(&ExternalSpeedLimiter::reconfigure, this, _1, _2));
+
+  // Create the subscriber
+  subscriber_ = private_nh.subscribe("limit", 10, &ExternalSpeedLimiter::msgCallback, this);
+}
+
+bool ExternalSpeedLimiter::calculateLimits(double& max_allowed_linear_vel, double& max_allowed_angular_vel) {
+  // Reset the maximum allowed velocity
+  max_allowed_linear_vel = max_linear_velocity_;
+  max_allowed_angular_vel = max_angular_velocity_;
+
+  if (ros::Time::now() - last_msg_time_ > ros::Duration(params_.timeout)) {
+    ROS_INFO_THROTTLE(0.5, "External speed limiter timeout.");
+    return true;
+  }
+
+  if (msg.max_linear_velocity >= 0) {
+    max_allowed_linear_vel = std::min(last_msg_.max_linear_velocity, max_linear_velocity_);  
+  }
+  
+  if (msg.max_angular_velocity >= 0) {
+    max_allowed_angular_vel = std::min(last_msg_.max_angular_velocity, max_angular_velocity_);
+  }
+
+  ROS_DEBUG_THROTTLE(0.2, "Setting external max speed to %f, %f", max_allowed_linear_vel, max_allowed_angular_vel);
+
+  return true;
+}
+
+} /* namespace base_local_planner */
