484b32637bdb3419676a6b696a599d7b368036d4
Pavlo Kolomiiets <pkolomiets@gmail.com>
2018-03-16 11:01:53 +0200

    LocalPlannerUtil keeps time for TF transforms.

--- base_local_planner/include/base_local_planner/goal_functions.h
+++ base_local_planner/include/base_local_planner/goal_functions.h
@@ -102,6 +102,7 @@ namespace base_local_planner {
       const geometry_msgs::PoseStamped& global_robot_pose,
       const costmap_2d::Costmap2D& costmap,
       const std::string& global_frame,
+      const ros::Time& time,
       std::vector<geometry_msgs::PoseStamped>& transformed_plan);
 
   /**
@@ -115,6 +116,7 @@ namespace base_local_planner {
   bool getGoalPose(const tf2_ros::Buffer& tf,
       const std::vector<geometry_msgs::PoseStamped>& global_plan,
       const std::string& global_frame,
+      const ros::Time& time,
       geometry_msgs::PoseStamped &goal_pose);
 
   /**
@@ -134,6 +136,7 @@ namespace base_local_planner {
       const std::vector<geometry_msgs::PoseStamped>& global_plan,
       const costmap_2d::Costmap2D& costmap,
       const std::string& global_frame,
+      const ros::Time& time,
       geometry_msgs::PoseStamped& global_pose,
       const nav_msgs::Odometry& base_odom,
       double rot_stopped_vel, double trans_stopped_vel,
