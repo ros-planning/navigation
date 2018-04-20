940c8f0214d127931e73ecc3762c5c4c7f4c6741
Ramon Wijnands <ramon@ruvu.nl>
2018-04-20 17:32:16 +0200

    Add const to getLocalPlan

--- base_local_planner/include/base_local_planner/local_planner_util.h
+++ base_local_planner/include/base_local_planner/local_planner_util.h
@@ -95,7 +95,7 @@ public:
 
   bool setPlan(const std::vector<geometry_msgs::PoseStamped>& orig_global_plan);
 
-  bool getLocalPlan(tf::Stamped<tf::Pose>& global_pose, std::vector<geometry_msgs::PoseStamped>& transformed_plan);
+  bool getLocalPlan(const tf::Stamped<tf::Pose>& global_pose, std::vector<geometry_msgs::PoseStamped>& transformed_plan);
 
   costmap_2d::Costmap2D* getCostmap();
 
