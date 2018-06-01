495a05a569875dab7dabee6d5cddf763c9aa94f8
Martin Ganeff <mganeff@lely.com>
2018-06-01 16:19:05 +0200

    Upscaled pre-hydro parameter info message to warning and added costmap-name

--- costmap_2d/src/costmap_2d_ros.cpp
+++ costmap_2d/src/costmap_2d_ros.cpp
@@ -135,7 +135,7 @@ Costmap2DROS::Costmap2DROS(std::string name, tf::TransformListener& tf) :
     {
       std::string pname = static_cast<std::string>(my_list[i]["name"]);
       std::string type = static_cast<std::string>(my_list[i]["type"]);
-      ROS_INFO("Using plugin \"%s\"", pname.c_str());
+      ROS_INFO("%s: Using plugin \"%s\"", name_.c_str(), pname.c_str());
 
       boost::shared_ptr<Layer> plugin = plugin_loader_.createInstance(type);
       layered_costmap_->addPlugin(plugin);
@@ -203,7 +203,7 @@ Costmap2DROS::~Costmap2DROS()
 
 void Costmap2DROS::loadOldParameters(ros::NodeHandle& nh)
 {
-  ROS_INFO("Loading from pre-hydro parameter style");
+  ROS_WARN("%s: Parameter \"plugins\" not provided, loading pre-Hydro parameters", name_.c_str());
   bool flag;
   std::string s;
   std::vector < XmlRpc::XmlRpcValue > plugins;
