ca6d183a0ef22582a93547cd3d9a1c3bdfddb2a3
Jorge Santos Simón <jsantossimon@gmail.com>
2019-06-18 22:09:31 +0200

    Provide different negative values for unknown and out-of-map costs (#833)

--- base_local_planner/include/base_local_planner/costmap_model.h
+++ base_local_planner/include/base_local_planner/costmap_model.h
@@ -52,7 +52,7 @@ namespace base_local_planner {
       /**
        * @brief  Constructor for the CostmapModel
        * @param costmap The costmap that should be used
-       * @return 
+       * @return
        */
       CostmapModel(const costmap_2d::Costmap2D& costmap);
 
@@ -68,7 +68,10 @@ namespace base_local_planner {
        * @param  footprint The specification of the footprint of the robot in world coordinates
        * @param  inscribed_radius The radius of the inscribed circle of the robot
        * @param  circumscribed_radius The radius of the circumscribed circle of the robot
-       * @return Positive if all the points lie outside the footprint, negative otherwise
+       * @return Positive if all the points lie outside the footprint, negative otherwise:
+       *            -1 if footprint covers at least a lethal obstacle cell, or
+       *            -2 if footprint covers at least a no-information cell, or
+       *            -3 if footprint is [partially] outside of the map
        */
       virtual double footprintCost(const geometry_msgs::Point& position, const std::vector<geometry_msgs::Point>& footprint,
           double inscribed_radius, double circumscribed_radius);
@@ -85,8 +88,8 @@ namespace base_local_planner {
 
       /**
        * @brief  Checks the cost of a point in the costmap
-       * @param x The x position of the point in cell coordinates 
-       * @param y The y position of the point in cell coordinates 
+       * @param x The x position of the point in cell coordinates
+       * @param y The y position of the point in cell coordinates
        * @return A positive cost for a legal point... negative otherwise
        */
       double pointCost(int x, int y) const;
