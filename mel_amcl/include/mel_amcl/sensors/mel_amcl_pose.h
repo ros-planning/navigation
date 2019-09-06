19aa05a4be2f2ee2ebc0836190f124d221d7f363
MikHut <mhutchinson@sagarobotics.com>
2019-09-06 10:08:55 +0100

    update file information

--- mel_amcl/include/mel_amcl/sensors/mel_amcl_pose.h
+++ mel_amcl/include/mel_amcl/sensors/mel_amcl_pose.h
@@ -1,28 +1,8 @@
-/*
- *  Player - One Hell of a Robot Server
- *  Copyright (C) 2000  Brian Gerkey et al.
- *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License as published by the Free Software Foundation; either
- *  version 2.1 of the License, or (at your option) any later version.
- *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- */
 ///////////////////////////////////////////////////////////////////////////
 //
-// Desc: Odometry sensor model for AMCL
-// Author: Andrew Howard
-// Date: 17 Aug 2003
-// CVS: $Id: amcl_odom.h 4135 2007-08-23 19:58:48Z gerkey $
+// Desc: Position sensor model for AMCL
+// Author: Michael Hutchinson
+// Date: 19 Aug 2019
 //
 ///////////////////////////////////////////////////////////////////////////
 
