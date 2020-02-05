93102e62e0afcad1e496a1dbf5504d6548f09941
Bob Henz <robert_henz@jabil.com>
2020-02-05 08:11:01 -0500

    Enhance script for getting the current pose distance
     * Allow it to take in x, y, z offset values
     * The service no longer requires the pose to be in the "odom" frame, so
       remove all that transform code.

--- costmap_3d/scripts/get_current_pose_distance_3d.py
+++ costmap_3d/scripts/get_current_pose_distance_3d.py
@@ -4,58 +4,53 @@ Test the performance of 3D costmap by sending a veyr large get plan cost service
 """
 
 import sys
-import math
-import random
+#import math
+#import random
 import rospy
-import tf2_ros
-import tf2_geometry_msgs
-import tf.transformations
+#import tf2_ros
+#import tf2_geometry_msgs
+#import tf.transformations
 import costmap_3d.srv
 import geometry_msgs.msg
-import copy
+#import copy
 
 
 if __name__ == "__main__":
+    x = 0
+    if len(sys.argv) > 1:
+        x = float(sys.argv[1])
+    y = 0
+    if len(sys.argv) > 2:
+        y = float(sys.argv[2])
+    z = 0
+    if len(sys.argv) > 3:
+        z = float(sys.argv[3])
 
     rospy.init_node("test_costmap3d_get_plan_cost_performance", anonymous=True)
 
-    tfBuffer = tf2_ros.Buffer()
-    tfListener = tf2_ros.TransformListener(tfBuffer)
-
-    try:
-        xform = tfBuffer.lookup_transform('odom', 'base_footprint',
-                rospy.Time(0.0), rospy.Duration(10.0))
-
-    except tf2_ros.TransformException as e:
-        rospy.logerr("Cannot determine base footprint transform")
-        sys.exit(1)
-
     get_cost_srv = rospy.ServiceProxy("/move_base/local_costmap/get_plan_cost_3d",
-            costmap_3d.srv.GetPlanCost3DService)
-    pose_array = geometry_msgs.msg.PoseArray()
-    pose_array.header.frame_id = "odom"
-    pose_array.header.stamp = rospy.Time.now()
+                                      costmap_3d.srv.GetPlanCost3DService)
     req = costmap_3d.srv.GetPlanCost3DServiceRequest()
     req.lazy = False
     req.buffered = True
-    req.header.frame_id = "odom"
-    req.header.stamp = pose_array.header.stamp
+    req.header.stamp = rospy.Time.now()
     req.cost_query_mode = costmap_3d.srv.GetPlanCost3DServiceRequest.COST_QUERY_MODE_DISTANCE
-    req.footprint_mesh_resource = ""
-#    req.footprint_mesh_resource = "package://ant_description/meshes/robot-get-close-to-obstacle-backward.stl"
-#    req.padding = 0.0
-    req.padding = float('NaN')
+    # req.footprint_mesh_resource = "package://ant_description/meshes/robot-get-close-to-obstacle-backward.stl"
+    # req.padding = float('NaN')
+    # req.footprint_mesh_resource = ""
+    # req.padding = 0.0
     pose = geometry_msgs.msg.PoseStamped()
     pose.header.frame_id = "base_footprint"
-    pose.pose.position.x = 0.0
-    pose.pose.position.y = 0.0
-    pose.pose.position.z = 0.0
+    pose.header.stamp = rospy.Time.now()
+    pose.pose.position.x = x
+    pose.pose.position.y = y
+    pose.pose.position.z = z
     pose.pose.orientation.x = 0.0
     pose.pose.orientation.y = 0.0
     pose.pose.orientation.z = 0.0
     pose.pose.orientation.w = 1.0
-    req.poses.append(tf2_geometry_msgs.do_transform_pose(pose, xform))
-#    rospy.loginfo("Request: " + str(req))
+    req.poses.append(pose)
+    rospy.loginfo("Request: " + str(req))
     res = get_cost_srv(req)
     rospy.loginfo("Result: " + str(res))
     rospy.loginfo("Number of lethals: " + str(len(res.lethal_indices)))
