7ef48e4e2688b6c5f1902307a5bd72f3e869b07b
ChristofDubs <christof.dubs@gmx.ch>
2020-02-07 12:27:27 +0900

    simplify prefer_forward_cost_function to just punish backwards motion

--- base_local_planner/src/prefer_forward_cost_function.cpp
+++ base_local_planner/src/prefer_forward_cost_function.cpp
@@ -15,14 +15,9 @@ namespace base_local_planner {
 double PreferForwardCostFunction::scoreTrajectory(Trajectory &traj) {
   // backward motions bad on a robot without backward sensors
   if (traj.xv_ < 0.0) {
-    return penalty_;
+    return -traj.xv_;
   }
-  // strafing motions also bad on such a robot
-  if (traj.xv_ < 0.1 && fabs(traj.thetav_) < 0.2) {
-    return penalty_;
-  }
-  // the more we rotate, the less we progress forward
-  return fabs(traj.thetav_) * 10;
+  return 0;
 }
 
 } /* namespace base_local_planner */
