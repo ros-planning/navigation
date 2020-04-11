0861bb80cf584d0cb4dc7574a459e014f8c77069
C. Andy Martin <cam@badger-technologies.com>
2020-04-11 06:36:00 -0900

    cosmtap_3d: alter script for pose to get derivative
    Alter the get current pose to sample small deltas to estimate
    the derivative. This is useful when testing signed distance.

--- costmap_3d/scripts/get_current_pose_distance_3d.py
+++ costmap_3d/scripts/get_current_pose_distance_3d.py
@@ -12,7 +12,7 @@ import rospy
 #import tf.transformations
 import costmap_3d.srv
 import geometry_msgs.msg
-#import copy
+import copy
 
 
 if __name__ == "__main__":
@@ -34,7 +34,7 @@ if __name__ == "__main__":
     req.lazy = False
     req.buffered = False
     req.header.stamp = rospy.Time.now()
-    req.cost_query_mode = costmap_3d.srv.GetPlanCost3DServiceRequest.COST_QUERY_MODE_DISTANCE
+    req.cost_query_mode = costmap_3d.srv.GetPlanCost3DServiceRequest.COST_QUERY_MODE_SIGNED_DISTANCE
     # req.footprint_mesh_resource = "package://ant_description/meshes/robot-get-close-to-obstacle-backward.stl"
     # req.padding = float('NaN')
     # req.footprint_mesh_resource = ""
@@ -49,13 +49,23 @@ if __name__ == "__main__":
     pose.pose.orientation.y = 0.0
     pose.pose.orientation.z = 0.0
     pose.pose.orientation.w = 1.0
-    req.poses.append(pose)
-    req.poses.append(pose)
-    req.poses.append(pose)
-    req.cost_query_regions = [costmap_3d.srv.GetPlanCost3DServiceRequest.COST_QUERY_REGION_ALL,
-                              costmap_3d.srv.GetPlanCost3DServiceRequest.COST_QUERY_REGION_LEFT,
-                              costmap_3d.srv.GetPlanCost3DServiceRequest.COST_QUERY_REGION_RIGHT]
+    req.poses.append(copy.deepcopy(pose))
+    pose.pose.position.x = x + 1e-5
+    req.poses.append(copy.deepcopy(pose))
+    pose.pose.position.x = x
+    pose.pose.position.y = y + 1e-5
+    req.poses.append(copy.deepcopy(pose))
+    pose.pose.position.y = y
+    pose.pose.orientation.z += 1e-5
+    pose.pose.orientation.w -= 1e-5
+    req.poses.append(copy.deepcopy(pose))
+#    req.cost_query_regions = [costmap_3d.srv.GetPlanCost3DServiceRequest.COST_QUERY_REGION_ALL,
+#                              costmap_3d.srv.GetPlanCost3DServiceRequest.COST_QUERY_REGION_LEFT,
+#                              costmap_3d.srv.GetPlanCost3DServiceRequest.COST_QUERY_REGION_RIGHT]
     rospy.loginfo("Request: " + str(req))
     res = get_cost_srv(req)
     rospy.loginfo("Result: " + str(res))
     rospy.loginfo("Number of lethals: " + str(len(res.lethal_indices)))
+    rospy.loginfo(" dx: " + str(1e5 * (res.pose_costs[1]-res.pose_costs[0])))
+    rospy.loginfo(" dy: " + str(1e5 * (res.pose_costs[2]-res.pose_costs[0])))
+    rospy.loginfo(" dt: " + str(1e5 * (res.pose_costs[3]-res.pose_costs[0])))
