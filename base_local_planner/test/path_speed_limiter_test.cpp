9e3f634137daff67fe91b69966c3c10649733cac
27Apoorva <agupta1@6river.com>
2020-05-11 18:13:05 -0400

    feat: one more test case

--- base_local_planner/test/path_speed_limiter_test.cpp
+++ base_local_planner/test/path_speed_limiter_test.cpp
@@ -29,6 +29,38 @@ std::vector<geometry_msgs::PoseStamped> createPlan() {
     return out;
 }
 
+/**
+ * createPlan() creates a vector of poses
+ * for 10m but global path is curved
+*/
+std::vector<geometry_msgs::PoseStamped> createCurvedPlan() {
+    std::vector<geometry_msgs::PoseStamped> out;
+
+    for (size_t i = 0; i < 10; i++) {
+        geometry_msgs::PoseStamped p0;
+
+        if( i > 5) {
+            p0.pose.position.x = 5;
+            p0.pose.position.y = i;
+            p0.pose.position.z = 0;
+            p0.pose.orientation.x = 0;
+            p0.pose.orientation.y = 0;
+            p0.pose.orientation.z = 0.7;
+            p0.pose.orientation.w = 0.7;
+        } else {
+            p0.pose.position.x = (i+1);
+            p0.pose.position.y = 1;
+            p0.pose.orientation.x = 0;
+            p0.pose.orientation.y = 0;
+            p0.pose.orientation.z = 0;
+            p0.pose.orientation.w = 1;
+        }
+        
+        out.push_back(p0);
+    }
+    return out;
+}
+
 namespace base_local_planner {
 /**
  * setup the config for path speed limiter(psl)
@@ -47,20 +79,20 @@ PathSpeedLimiter setupAndCreatePSL() {
     cfg.max_heading_difference = 1.0;
     cfg.min_linear_velocity = 0.12;
     path_speed_limiter.reconfigure(cfg);
-    path_speed_limiter.setPlan(createPlan());
+    
     return path_speed_limiter;
 }
 
 /**
  * Test 1: Check which speed limiter to use
- * Test 2: Check  if path starts behind the robot, 
+ * Test 2: If path starts behind the robot, 
  * then calculateLimitsFromPathAndPose shouldn't calculate heading diff
  * and apply speed limit to it.
 */
 TEST(PathSpeedLimiter, heading_diff)
 {
     auto psl = setupAndCreatePSL();
-    
+    psl.setPlan(createPlan());
     double v_lim = 0, w_lim = 0;
     
     geometry_msgs::PoseStamped currentRobotPose;
@@ -76,6 +108,30 @@ TEST(PathSpeedLimiter, heading_diff)
 
     EXPECT_EQ(psl.getName(), std::string("Path"));
     EXPECT_EQ(v_lim, 1.0);
+}
+
+/**
+ * Test 1: If path is curved, path limiter should limit the velocity
+ * by setting the linear velocity to min_linear_velocity 
+ * set by PathSpeedLimiterConfig in setupAndCreatePSL function 
+*/
+TEST(PathSpeedLimiter, limiter_in_action)
+{
+    auto psl = setupAndCreatePSL();
+    psl.setPlan(createCurvedPlan());
+    
+    double v_lim = 0, w_lim = 0;
+    geometry_msgs::PoseStamped currentRobotPose;
+    currentRobotPose.pose.position.x = 4;
+    currentRobotPose.pose.position.y = 1;
+    currentRobotPose.pose.position.z = 0;
+    currentRobotPose.pose.orientation.x = 0;
+    currentRobotPose.pose.orientation.y = 0;
+    currentRobotPose.pose.orientation.z = 0.5;
+    currentRobotPose.pose.orientation.w = 0.8660254;
+
+    bool result = psl.calculateLimitsFromPathAndPose(v_lim, w_lim, currentRobotPose);
+    EXPECT_EQ(v_lim, 0.12);
 }
 
 }
