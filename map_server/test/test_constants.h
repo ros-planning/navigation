44236ed0218c87296a7a4fadd05c4592a062706b
David V. Lu <davidvlu@gmail.com>
2020-07-03 11:42:49 -0400

    Initial map_server map_mode tests

--- map_server/test/test_constants.h
+++ map_server/test/test_constants.h
@@ -39,5 +39,6 @@ extern const char g_valid_image_content[];
 extern const char* g_valid_png_file;
 extern const char* g_valid_bmp_file;
 extern const float g_valid_image_res;
+extern const char* g_spectrum_png_file;
 
 #endif
