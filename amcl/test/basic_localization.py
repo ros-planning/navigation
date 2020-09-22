3c16b37504dc178c3075345ed88d681a6f83cc2e
Christian Clauss <cclauss@me.com>
2020-09-22 02:56:17 +0200

    Use print() function in both Python 2 and Python 3 in basic_localization.py  (#1011)

--- amcl/test/basic_localization.py
+++ amcl/test/basic_localization.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 import sys
 import time
 from math import fmod, pi
@@ -25,10 +27,10 @@ class TestBasicLocalization(unittest.TestCase):
             if t.header.frame_id == 'map':
                 self.tf = t.transform
                 (a_curr, a_diff) = self.compute_angle_diff()
-                print 'Curr:\t %16.6f %16.6f %16.6f' % (self.tf.translation.x, self.tf.translation.y, a_curr)
-                print 'Target:\t %16.6f %16.6f %16.6f' % (self.target_x, self.target_y, self.target_a)
-                print 'Diff:\t %16.6f %16.6f %16.6f' % (
-                    abs(self.tf.translation.x - self.target_x), abs(self.tf.translation.y - self.target_y), a_diff)
+                print('Curr:\t %16.6f %16.6f %16.6f' % (self.tf.translation.x, self.tf.translation.y, a_curr))
+                print('Target:\t %16.6f %16.6f %16.6f' % (self.target_x, self.target_y, self.target_a))
+                print('Diff:\t %16.6f %16.6f %16.6f' % (
+                    abs(self.tf.translation.x - self.target_x), abs(self.tf.translation.y - self.target_y), a_diff))
 
     def compute_angle_diff(self):
         rot = self.tf.rotation
@@ -47,28 +49,28 @@ class TestBasicLocalization(unittest.TestCase):
         target_time = float(sys.argv[7])
 
         if global_localization == 1:
-            #print 'Waiting for service global_localization'
+            #print('Waiting for service global_localization')
             rospy.wait_for_service('global_localization')
             global_localization = rospy.ServiceProxy('global_localization', Empty)
             global_localization()
 
         rospy.init_node('test', anonymous=True)
         while(rospy.rostime.get_time() == 0.0):
-            #print 'Waiting for initial time publication'
+            #print('Waiting for initial time publication')
             time.sleep(0.1)
         start_time = rospy.rostime.get_time()
         # TODO: This should be replace by a pytf listener
         rospy.Subscriber('/tf', TFMessage, self.tf_cb)
 
         while (rospy.rostime.get_time() - start_time) < target_time:
-            #print 'Waiting for end time %.6f (current: %.6f)'%(target_time,(rospy.rostime.get_time() - start_time))
+            #print('Waiting for end time %.6f (current: %.6f)'%(target_time,(rospy.rostime.get_time() - start_time)))
             time.sleep(0.1)
 
         (a_curr, a_diff) = self.compute_angle_diff()
-        print 'Curr:\t %16.6f %16.6f %16.6f' % (self.tf.translation.x, self.tf.translation.y, a_curr)
-        print 'Target:\t %16.6f %16.6f %16.6f' % (self.target_x, self.target_y, self.target_a)
-        print 'Diff:\t %16.6f %16.6f %16.6f' % (
-            abs(self.tf.translation.x - self.target_x), abs(self.tf.translation.y - self.target_y), a_diff)
+        print('Curr:\t %16.6f %16.6f %16.6f' % (self.tf.translation.x, self.tf.translation.y, a_curr))
+        print('Target:\t %16.6f %16.6f %16.6f' % (self.target_x, self.target_y, self.target_a))
+        print('Diff:\t %16.6f %16.6f %16.6f' % (
+            abs(self.tf.translation.x - self.target_x), abs(self.tf.translation.y - self.target_y), a_diff))
         self.assertNotEquals(self.tf, None)
         self.assertTrue(abs(self.tf.translation.x - self.target_x) <= tolerance_d)
         self.assertTrue(abs(self.tf.translation.y - self.target_y) <= tolerance_d)
