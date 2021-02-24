4f80d13dd0af51e60a7dbef13724c8df6d32c8bc
nobunoby <nono.ao@keio.jp>
2021-02-24 15:37:25 +0900

    bug fix

--- costmap_2d/include/costmap_2d/costmap_2d_ros.h
+++ costmap_2d/include/costmap_2d/costmap_2d_ros.h
@@ -49,6 +49,7 @@
 #include <dynamic_reconfigure/server.h>
 #include <pluginlib/class_loader.hpp>
 #include <tf2/LinearMath/Transform.h>
+#include<std_msgs/Int32.h>
 
 class SuperValue : public XmlRpc::XmlRpcValue
 {
