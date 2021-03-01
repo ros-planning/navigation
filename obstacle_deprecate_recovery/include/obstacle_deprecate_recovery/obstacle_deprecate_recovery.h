69dde8dfd0fef7ddb653cd311562fb414db7abe7
HayatoT1209 <t_hayato1209@yahoo.co.jp>
2021-03-01 16:00:21 +0900

    Cleaned unused portions of the code

--- obstacle_deprecate_recovery/include/obstacle_deprecate_recovery/obstacle_deprecate_recovery.h
+++ obstacle_deprecate_recovery/include/obstacle_deprecate_recovery/obstacle_deprecate_recovery.h
@@ -1,9 +1,7 @@
 #ifndef OBSTACLE_DEPRECATE_RECOVERY_H_
 #define OBSTACLE_DEPRECATE_RECOVERY_H_
-#include <nav_core/recovery_behavior.h>
-#include <costmap_2d/costmap_2d_ros.h>
 #include <ros/ros.h>
-#include <costmap_2d/costmap_layer.h>
+#include <nav_core/recovery_behavior.h>
 #include <base_local_planner/costmap_model.h>
 
 namespace obstacle_deprecate_recovery{
@@ -18,9 +16,8 @@ namespace obstacle_deprecate_recovery{
             //~ObstacleDeprecateRecovery();
 
         private:
-            costmap_2d::Costmap2DROS* global_costmap_, *local_costmap_;
             bool initialized_;
-            double min_inflation_, min_occdist_scale_, max_sim_time_;
+            double min_occdist_scale_, max_sim_time_;
             double occdist_scale_old_, sim_time_old_;
     };
 };
--- obstacle_deprecate_recovery/src/obstacle_deprecate_recovery.cpp
+++ obstacle_deprecate_recovery/src/obstacle_deprecate_recovery.cpp
@@ -5,20 +5,15 @@
 PLUGINLIB_EXPORT_CLASS(obstacle_deprecate_recovery::ObstacleDeprecateRecovery, nav_core::RecoveryBehavior);
 
 namespace obstacle_deprecate_recovery{
-    ObstacleDeprecateRecovery::ObstacleDeprecateRecovery(): global_costmap_(NULL), local_costmap_(NULL), initialized_(false) 
+    ObstacleDeprecateRecovery::ObstacleDeprecateRecovery(): initialized_(false) 
     {
     }
 
     void ObstacleDeprecateRecovery::initialize(std::string name, tf2_ros::Buffer* tf, costmap_2d::Costmap2DROS* global_costmap, costmap_2d::Costmap2DROS* local_costmap){
         if(!initialized_){
-            global_costmap_ = global_costmap;
-            local_costmap_ = local_costmap;
-
             // get some parameters from the parameter server
             ros::NodeHandle private_nh("~/" + name);
-            ros::NodeHandle blp_nh("~/TrajectoryPlannerROS");
 
-            private_nh.param("min_inflation", min_inflation_, 0.1);
             private_nh.param("min_occdist_scale", min_occdist_scale_, 0.0);
             private_nh.param("max_sim_time", max_sim_time_, 4.0);
 
