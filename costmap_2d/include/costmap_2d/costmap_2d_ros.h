f4f8518c46151348611f9e247f05612003f64975
nobunoby <nono.ao@keio.jp>
2021-03-03 17:57:17 +0900

    mismatch resolved

--- costmap_2d/include/costmap_2d/costmap_2d_ros.h
+++ costmap_2d/include/costmap_2d/costmap_2d_ros.h
@@ -281,7 +281,7 @@ private:
   costmap_2d::Costmap2DConfig old_config_;
 };
 
-std::vector<geometry_msgs::Point> return_footprint(const XmlRpc::XmlRpcValue& footprint_xmlrpc);
+std::vector<geometry_msgs::Point> return_footprint(XmlRpc::XmlRpcValue footprint_xmlrpc);
 // class Costmap2DROS
 }  // namespace costmap_2d
 
