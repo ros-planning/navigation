620837934956dbf757467820154e21c88f988697
n-ando <Noriaki.Ando@gmail.com>
2021-03-06 14:36:02 +0900

    Fixed errors that virtual destructors are not declared.

--- global_planner/include/global_planner/expander.h
+++ global_planner/include/global_planner/expander.h
@@ -48,6 +48,7 @@ class Expander {
                 unknown_(true), lethal_cost_(253), neutral_cost_(50), factor_(3.0), p_calc_(p_calc) {
             setSize(nx, ny);
         }
+        virtual Expander() {}
         virtual bool calculatePotentials(unsigned char* costs, double start_x, double start_y, double end_x, double end_y,
                                         int cycles, float* potential) = 0;
 
