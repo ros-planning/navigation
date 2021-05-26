62c96475de21a36f35bd335d225cff260140ea20
HayatoT1209 <t_hayato1209@yahoo.co.jp>
2021-05-26 15:48:43 +0900

    Fixed a bug

--- rotate_small_recovery/src/rotate_small_recovery.cpp
+++ rotate_small_recovery/src/rotate_small_recovery.cpp
@@ -26,13 +26,13 @@ void RotateSmallRecovery::initialize(std::string name, tf2_ros::Buffer*,
     // we'll simulate every degree by default
     private_nh.param("sim_granularity", sim_granularity_, 0.017);
     private_nh.param("frequency", frequency_, 20.0);
-    angle_to_rot_ *= M_PI/180.0;
 
     acc_lim_th_ = nav_core::loadParameterWithDeprecation(blp_nh, "acc_lim_theta", "acc_lim_th", 3.2);
     max_rotational_vel_ = nav_core::loadParameterWithDeprecation(blp_nh, "max_vel_theta", "max_rotational_vel", 1.0);
     min_rotational_vel_ = nav_core::loadParameterWithDeprecation(blp_nh, "min_in_place_vel_theta", "min_in_place_rotational_vel", 0.4);
     blp_nh.param("yaw_goal_tolerance", tolerance_, 0.10);
     blp_nh.param("rotate_small_angle", angle_to_rot_, 30.0);
+    angle_to_rot_ *= M_PI/180.0;
 
     world_model_ = new base_local_planner::CostmapModel(*local_costmap_->getCostmap());
 
