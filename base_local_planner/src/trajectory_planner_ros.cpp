0da5b68b62153ad8684ef334d618ac3c5d46bff5
Lucas Walter <wsacul@gmail.com>
2022-12-25 11:18:29 -0800

    use pluginlib hpp header instead of deprecated .h

--- base_local_planner/src/trajectory_planner_ros.cpp
+++ base_local_planner/src/trajectory_planner_ros.cpp
@@ -48,7 +48,7 @@
 
 #include <ros/console.h>
 
-#include <pluginlib/class_list_macros.h>
+#include <pluginlib/class_list_macros.hpp>
 
 #include <base_local_planner/goal_functions.h>
 #include <nav_msgs/Path.h>
--- global_planner/src/planner_core.cpp
+++ global_planner/src/planner_core.cpp
@@ -36,7 +36,7 @@
  *         David V. Lu!!
  *********************************************************************/
 #include <global_planner/planner_core.h>
-#include <pluginlib/class_list_macros.h>
+#include <pluginlib/class_list_macros.hpp>
 #include <costmap_2d/cost_values.h>
 #include <costmap_2d/costmap_2d.h>
 
