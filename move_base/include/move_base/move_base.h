2ffffc67eea5399b3313fb0f827e40aeecfa1478
Remma Matsuda <remma.matsuda@lexxpluss.com>
2023-06-02 15:16:25 +0900

    [AMRSW-94] add a parameter for frequent recovery.

--- move_base/include/move_base/move_base.h
+++ move_base/include/move_base/move_base.h
@@ -206,6 +206,7 @@ namespace move_base {
       std::vector<boost::shared_ptr<nav_core::RecoveryBehavior> > recovery_behaviors_, recovery_behaviors_carrying_;
       unsigned int recovery_index_;
       bool recovery_flag_ = false;
+      bool frequent_recovery_motion_ = false;
 
       geometry_msgs::PoseStamped global_pose_;
       std_msgs::String amr_status_msg_;
